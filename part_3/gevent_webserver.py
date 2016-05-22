4836fc2d404281bb114d8a8eb8216a933fa60db7
c1ay <liukaiash@gmail.com>
2016-05-22 21:48:41 +0800

    init gevent WSGI file

---
+++ part_3/gevent_webserver.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+import socket
+import gevent
+
+class EPOLLServer(object):
+
+    def __init__(self):
+        pass
+
+    def setup_app(self):
+        pass
+
+    def server(self):
+        pass
